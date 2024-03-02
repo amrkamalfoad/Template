7d05468e4ceda055292c72a8183d045ac3c29131
amr kamal <amrfoad6@gmail.com>
2024-03-02 21:25:57 +0200

    added links

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+//scroll up Button
 let container=document.querySelector(".skills-container");
 let spans=document.querySelectorAll(".skills-container .skill-div .skills>div .indication span");
 let up=document.getElementsByClassName('up')[0];
@@ -21,8 +22,8 @@ up.onclick=function(){
         behavior:"smooth"
     })
 }
+// by click on otherLinks in nav a dropdrown list appear
 let dropListNav=document.getElementsByClassName('megainside')[0];
-
 let otherLinks=document.getElementsByClassName('links')[0];
 console.log(otherLinks)
 otherLinks.addEventListener("click",function(){
@@ -34,4 +35,105 @@ otherLinks.addEventListener("click",function(){
         dropListNav.style.display = "none";
       }
 })
+//links on click scroll to container
+let articles=document.getElementById('articles');
+let divarticles=document.getElementsByClassName('articles')[0];
+let gallery=document.getElementById('gallery');
+let divgallery=document.getElementsByClassName('gallery-container')[0];
+let features=document.getElementById('features');
+let divfeatures=document.getElementsByClassName('features')[0];
+let testimonals=document.getElementById('testimonials');
+let divtestimonals=document.getElementsByClassName('test')[0];
+let team=document.getElementById('team');
+let divteammembers=document.getElementsByClassName('team-members')[0];
+let services=document.getElementById('services');
+let divservices=document.getElementsByClassName('services-container')[0];
+let skills=document.getElementById('ourskills');
+let divskills=document.getElementsByClassName('skills-container')[0];
+let works=document.getElementById('howitworks');
+let divworks=document.getElementsByClassName('works-container')[0];
+let events=document.getElementById('events');
+let divevents=document.getElementsByClassName('events-container')[0];
+let plans=document.getElementById('plans');
+let divplans=document.getElementsByClassName('plans-container')[0];
+let videos=document.getElementById('videos');
+let divvideos=document.getElementsByClassName('videos-container')[0];
+let stats=document.getElementById('stats');
+let divstats=document.getElementsByClassName('stats-container')[0];
+let discount=document.getElementById('discount');
+let divdiscount=document.getElementsByClassName('discount-container')[0];
 
+articles.addEventListener('click',function(){
+    window.scrollTo({
+        top:divarticles.offsetTop,
+        behavior:"smooth"
+    })
+})
+gallery.addEventListener('click',function(){
+    window.scrollTo({
+        top:divgallery.offsetTop,
+        behavior:"smooth"
+    })
+})
+features.addEventListener('click',function(){
+    window.scrollTo({
+        top:divfeatures.offsetTop,
+        behavior:"smooth"
+    })
+})
+testimonals.addEventListener('click',function(){
+    window.scrollTo({
+        top:divtestimonals.offsetTop,
+        behavior:"smooth"
+    })
+})
+services.addEventListener('click',function(){
+    window.scrollTo({
+        top:divservices.offsetTop,
+        behavior:"smooth"
+    })
+})
+skills.addEventListener('click',function(){
+    window.scrollTo({
+        top:divskills.offsetTop,
+        behavior:"smooth"
+    })
+})
+works.addEventListener('click',function(){
+    window.scrollTo({
+        top:divworks.offsetTop,
+        behavior:"smooth"
+    })
+})
+events.addEventListener('click',function(){
+    window.scrollTo({
+        top:divevents.offsetTop,
+        behavior:"smooth"
+    })
+})
+plans.addEventListener('click',function(){
+    window.scrollTo({
+        top:divplans.offsetTop,
+        behavior:"smooth"
+    })
+})
+videos.addEventListener('click',function(){
+    window.scrollTo({
+        top:divvideos.offsetTop,
+        behavior:"smooth"
+    })
+})
+stats.addEventListener('click',function(){
+    window.scrollTo({
+        top:divstats.offsetTop,
+        behavior:"smooth"
+    })
+});
+discount.addEventListener('click',function(){
+    window.scrollTo({
+        top:divdiscount.offsetTop,
+        behavior:"smooth"
+    })
+});
+
+//
